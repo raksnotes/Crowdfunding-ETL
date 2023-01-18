4aba4e20415fca90b76d89f22b084d2d33e2f59b
Raksha Karthikeyan <116187123+raksnotes@users.noreply.github.com>
2023-01-17 21:29:59 -0500

    Update crowdfunding_db_schema.sql

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -69,7 +69,7 @@ REFERENCES "category" ("category_id");
 ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
 REFERENCES "subcategory" ("subcategory_id");
 
-ALTER TABLE "backers" ADD CONSTRAINT "fk_cf_id" 
+ALTER TABLE "backers2" ADD CONSTRAINT "fk_cf_id" 
 FOREIGN KEY("cf_id") 
 REFERENCES "campaign" ("cf_id"); 
 
